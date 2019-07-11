fa9e8321c06ae927f13c35e914515978089d80e2
JAHAIRA DE LOS ANGELES MORALES VARGAS <jahaira.morales@uhispano.ac.cr>
2019-07-11 17:34:07 -0600

    Interfaz 2

---
+++ ControlApp.Presentation/Controllers/CreateUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlApp.Presentation.Controllers
+{
+    public class CreateUserController : Controller
+    {
+        // GET: CreateUser
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+    }
+}
