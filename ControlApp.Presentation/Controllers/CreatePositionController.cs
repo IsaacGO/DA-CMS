fb06ed3d173a5db279233eefb4828035d9f94fa1
JAHAIRA DE LOS ANGELES MORALES VARGAS <jahaira.morales@uhispano.ac.cr>
2019-07-12 16:27:26 -0600

    Cambio Interface

---
+++ ControlApp.Presentation/Controllers/CreatePositionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlApp.Presentation.Controllers
+{
+    public class CreatePositionController : Controller
+    {
+        // GET: CreatePosition
+        public ActionResult CreatePosition()
+        {
+            return View();
+        }
+    }
+}
