56adeb498a964d49032c127f0ccc4188f32cfc9c
Maria Fernanda Villagra Miranda <maria.villagra@uhispano.ac.cr>
2019-07-27 13:09:24 -0600

    CRUD Note

---
+++ ControlApp.ApiCore/Management/NoteManagement.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControlApp.DataAccess.Crud;
+using ControlApp.Entities.Objects;
+using log4net;
+
+namespace ControlApp.ApiCore.Management
+{
+    public class NoteManagement : BaseManagement
+    {
+        public NoteManagement()
+        {
+            _factory = new CrudNote();
+        }
+        private static readonly ILog log = LogManager.GetLogger(System.Environment.MachineName);
+        public void Create(Note Obj)
+        {
+            try
+            {
+                _factory.Create(Obj);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+        }
+        public List<T> SuperRetrieve<T>()
+        {
+            var lst = new List<T>();
+            try
+            {
+                var ret = _factory.SuperRetrieve<Note>();
+                foreach (var obj in ret)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> SuperRetrieveById<T>(Note Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.SuperRetrieveById<Note>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> SuperRetrieveByNameDescrip<T>(Note Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.SuperRetrieveByName<Note>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> RetrieveAll<T>()
+        {
+            var lst = new List<T>();
+            try
+            {
+                var ret = _factory.RetrieveAll<Note>();
+                foreach (var obj in ret)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> RetrieveAllByNameDescrip<T>(Note Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.RetrieveByName<Note>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> RetrieveAllById<T>(Note Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.RetrieveById<Note>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> RetrieveAllByIdUser<T>(Note Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.RetrieveByIdUser<Note>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public void Update(Note Obj)
+        {
+            try
+            {
+                _factory.Update(Obj);
+            }
+
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+        }
+        public void Delete(Note Obj)
+        {
+            try
+            {
+                _factory.Delete(Obj);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+        }
+        public void Activate(Note Obj)
+        {
+            try
+            {
+                _factory.Activate(Obj);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+        }
+    }
+}
---
+++ ControlApp.DataAccess/Crud/CrudNote.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq; // Manipulacion de bases de datos SQL
+using System.Data.Linq.Mapping; // otra clase por importar para la Manipulacion
+using ControlApp.Entities.Objects;
+
+
+namespace ControlApp.DataAccess.Crud
+{
+    public class CrudNote : CrudFactory
+    {
+        sqlConnectionDataContext Context = new sqlConnectionDataContext();
+
+        public override bool Activate(BaseEntity entity)
+        {
+
+            var ObjNote = (Note)entity;
+            Context.SP_CRUD_USER((int)CrudActionEnum.Activate, ObjNote.Note_UpdateBy, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+            , ObjNote.Note_Date);
+            return true;
+        }
+        public override bool Create(BaseEntity entity)
+        {
+            var ObjNote = (Note)entity;
+            Context.SP_CRUD_USER((int)CrudActionEnum.Create, ObjNote.Note_CreateBy, ObjNote.ID_Note, ObjNote.Note_Title,ObjNote.Note_Content
+                ,ObjNote.Note_Date);
+            return true;
+        }
+        public override bool Delete(BaseEntity entity)
+        {
+
+            var ObjNote = (Note)entity;
+            Context.SP_CRUD_USER((int)CrudActionEnum.Delete, ObjNote.Note_UpdateBy, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+            , ObjNote.Note_Date);
+            return true;
+        }
+        public override List<T> SuperRetrieve<T>()
+        {
+            var ObjNote = new Note();
+            var lst = new List<T>();
+            List<Note> MyList = new List<Note>();
+            var Query = Context.SP_CRUD_NOTE((int)CrudActionEnum.SuperRetrieve, ObjNote.IdSession, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date).ToList();
+            foreach (SP_CRUD_NOTEResult Element in Query)
+            {
+                Note Obj = new Note(Element.ID_NOTE,Element.NOTE_TITLE,Element.NOTE_CONTENT,Element.NOTE_DATE,Element.NOTE_STATE,
+                    Element.NOTE_CREATEBY, Element.NOTE_UPDATEBY,Element.NOTE_CREATEDATE,Element.NOTE_UPDATEDATE);
+                MyList.Add(Obj);
+            }
+            if (MyList.Count > 0)
+            {
+                foreach (var obj in MyList)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            return lst;
+        }
+        public override List<T> SuperRetrieveById<T>(BaseEntity entity)
+        {
+            var ObjNote = new Note();
+            var lst = new List<T>();
+            List<Note> MyList = new List<Note>();
+            var Query = Context.SP_CRUD_NOTE((int)CrudActionEnum.SuperRetrieve, ObjNote.IdSession, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date).ToList();
+            foreach (SP_CRUD_NOTEResult Element in Query)
+            {
+                Note Obj = new Note(Element.ID_NOTE, Element.NOTE_TITLE, Element.NOTE_CONTENT, Element.NOTE_DATE, Element.NOTE_STATE,
+                    Element.NOTE_CREATEBY, Element.NOTE_UPDATEBY, Element.NOTE_CREATEDATE, Element.NOTE_UPDATEDATE);
+                MyList.Add(Obj);
+            }
+            if (MyList.Count > 0)
+            {
+                foreach (var obj in MyList)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            return lst;
+        }
+        public override List<T> SuperRetrieveByName<T>(BaseEntity entity)
+        {
+            var ObjNote = new Note();
+            var lst = new List<T>();
+            List<Note> MyList = new List<Note>();
+            var Query = Context.SP_CRUD_NOTE((int)CrudActionEnum.SuperRetrieve, ObjNote.IdSession, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date).ToList();
+            foreach (SP_CRUD_NOTEResult Element in Query)
+            {
+                Note Obj = new Note(Element.ID_NOTE, Element.NOTE_TITLE, Element.NOTE_CONTENT, Element.NOTE_DATE, Element.NOTE_STATE,
+                    Element.NOTE_CREATEBY, Element.NOTE_UPDATEBY, Element.NOTE_CREATEDATE, Element.NOTE_UPDATEDATE);
+                MyList.Add(Obj);
+            }
+            if (MyList.Count > 0)
+            {
+                foreach (var obj in MyList)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            return lst;
+        }
+        public override List<T> RetrieveAll<T>()
+        {
+            var ObjNote = new Note();
+            var lst = new List<T>();
+            List<Note> MyList = new List<Note>();
+            var Query = Context.SP_CRUD_NOTE((int)CrudActionEnum.RetrieveAll, ObjNote.IdSession, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date).ToList();
+            foreach (SP_CRUD_NOTEResult Element in Query)
+            {
+                Note Obj = new Note(Element.ID_NOTE, Element.NOTE_TITLE, Element.NOTE_CONTENT, Element.NOTE_DATE);
+                MyList.Add(Obj);
+            }
+            if (MyList.Count > 0)
+            {
+                foreach (var obj in MyList)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            return lst;
+        }
+        public override List<T> RetrieveById<T>(BaseEntity entity)
+        {
+            var ObjNote = new Note();
+            var lst = new List<T>();
+            List<Note> MyList = new List<Note>();
+            var Query = Context.SP_CRUD_NOTE((int)CrudActionEnum.RetrieveAll, ObjNote.IdSession, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date).ToList();
+            foreach (SP_CRUD_NOTEResult Element in Query)
+            {
+                Note Obj = new Note(Element.ID_NOTE, Element.NOTE_TITLE, Element.NOTE_CONTENT, Element.NOTE_DATE);
+                MyList.Add(Obj);
+            }
+            if (MyList.Count > 0)
+            {
+                foreach (var obj in MyList)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            return lst;
+        }
+        public override List<T> RetrieveByName<T>(BaseEntity entity)
+        {
+            var ObjNote = new Note();
+            var lst = new List<T>();
+            List<Note> MyList = new List<Note>();
+            var Query = Context.SP_CRUD_NOTE((int)CrudActionEnum.RetrieveAll, ObjNote.IdSession, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date).ToList();
+            foreach (SP_CRUD_NOTEResult Element in Query)
+            {
+                Note Obj = new Note(Element.ID_NOTE, Element.NOTE_TITLE, Element.NOTE_CONTENT, Element.NOTE_DATE);
+                MyList.Add(Obj);
+            }
+            if (MyList.Count > 0)
+            {
+                foreach (var obj in MyList)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            return lst;
+        }
+        public override bool Update(BaseEntity entity)
+        {
+
+            var ObjNote = (User)entity;
+            Context.SP_CRUD_NOTE((int)CrudActionEnum.Update, ObjNote.Note_UpdateBy, ObjNote.ID_Note, ObjNote.Note_Title, ObjNote.Note_Content
+                , ObjNote.Note_Date);
+            return true;
+        }
+        public override List<T> RetrieveByIdUser<T>(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
